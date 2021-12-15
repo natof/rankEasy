98d384fe27ef6d803926d7dcd97818d09208f374
Juan <76711419+JuqnDev@users.noreply.github.com>
2021-12-15 13:49:46 -0300

    Changes when getting the instance
    We use SingletonTrait for better code. In addition, to fix the error that poggit marks

--- src/natof/rankEasy/RankEasy.php
+++ src/natof/rankEasy/RankEasy.php
@@ -8,33 +8,27 @@ use pocketmine\permission\Permission;
 use pocketmine\permission\PermissionManager;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
+use pocketmine\utils\SingletonTrait;
 
 class RankEasy extends PluginBase
 {
-
-    private static RankEasy $instance;
+    use SingletonTrait;
 
     protected function onEnable(): void
     {
         @mkdir(self::getInstance()->getDataFolder() . "save");
         $this->saveResource("rank.yml");
+
         $this->getServer()->getPluginManager()->registerEvents(new PlayerListener(), $this);
         Server::getInstance()->getCommandMap()->register("rankEasy", new SetRank());
+
         PermissionManager::getInstance()->addPermission(new Permission("rankEasy.rank.use"));
         parent::onEnable();
     }
 
-    /**
-     * @return static
-     */
-    public static function getInstance(): self
-    {
-        return self::$instance;
-    }
-
     protected function onLoad(): void
     {
-        self::$instance = $this;
+        self::setInstance($this);
         parent::onLoad();
     }
 }
