50f36ae9143807b45a62b490117803ddd1579894
Juan <76711419+JuqnDev@users.noreply.github.com>
2021-12-15 13:51:01 -0300

    Update RankEasy.php

--- src/natof/rankEasy/RankEasy.php
+++ src/natof/rankEasy/RankEasy.php
@@ -4,8 +4,6 @@ namespace natof\rankEasy;
 
 use natof\rankEasy\command\SetRank;
 use natof\rankEasy\event\PlayerListener;
-use pocketmine\permission\Permission;
-use pocketmine\permission\PermissionManager;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\utils\SingletonTrait;
@@ -22,13 +20,13 @@ class RankEasy extends PluginBase
         $this->getServer()->getPluginManager()->registerEvents(new PlayerListener(), $this);
         Server::getInstance()->getCommandMap()->register("rankEasy", new SetRank());
 
-        PermissionManager::getInstance()->addPermission(new Permission("rankEasy.rank.use"));
         parent::onEnable();
     }
 
     protected function onLoad(): void
     {
         self::setInstance($this);
+
         parent::onLoad();
     }
 }
